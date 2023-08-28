b0e07fac7f13c186cd5d712c1127a6e573758b3e
yosefha4 <yosefha4@ac.sce.ac.il>
2023-08-28 10:32:34 +0300

    .env file

--- auth_python/db.py
+++ auth_python/db.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Load environment variables from .env file
 
-# MONGO_URI=mongodb+srv://yosefha4:yosefha4@cluster0.k4kiax3.mongodb.net/?retryWrites=true&w=majority
 
 #Database
 client = pymongo.MongoClient(os.environ.get("MONGO_URI"))
