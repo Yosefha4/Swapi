f8154e4f7401b45eb2124baf558e24680036b19d
yosefha4 <yosefha4@ac.sce.ac.il>
2023-08-28 10:30:16 +0300

    .env file

--- auth_python/db.py
+++ auth_python/db.py
@@ -1,6 +1,11 @@
+import os
 from flask import Flask
 import pymongo
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
 
 #Database
-client = pymongo.MongoClient('mongodb+srv://yosefha4:yosefha4@cluster0.k4kiax3.mongodb.net/?retryWrites=true&w=majority')
+client = pymongo.MongoClient(os.environ.get("MONGO_URI"))
 db = client.user_login_system
