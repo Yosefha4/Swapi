a886bc0099d2d14019825b2287367decf9468349
yosefha4 <yosefha4@ac.sce.ac.il>
2023-09-07 13:00:51 +0300

    Update apartment table (add ownerId and numOfParking fields

--- apartment_service/app.py
+++ apartment_service/app.py
@@ -8,13 +8,36 @@ CORS(app)
 
 def db_connection():
     conn = psycopg2.connect(database="flask_db",host="postgres",user="postgres",password="yosefha4",port="5432")
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS post_apart (
+                id serial PRIMARY KEY,
+                whichAction varchar(50),
+                userOwnerId varchar(400),
+                apType varchar(50),
+                apCity varchar(50),
+                apStreet varchar(50),
+                numOfRooms integer,
+                parkingNum integer,
+                moreDesc varchar(100),
+                builtInMeter integer,
+                price varchar(50),
+                availDate varchar(50),
+                apImages varchar[],
+                ownerName varchar(50),
+                ownerPhone varchar(50)
+                );
+            ''')    
+
+    cur.execute('''INSERT INTO post_apart (whichAction, userOwnerId ,apType, apCity, apStreet, numOfRooms, parkingNum, moreDesc, builtInMeter, price, availDate, apImages, ownerName, ownerPhone)
+    VALUES ('מכירה', 'testu2s2erid22owner22', 'דירה', 'תל אביב', 'קינג גורג', 5, 1, 'קומה 3, נוף לים', 170, '2999999', '1/12/2023', ARRAY['abcc','abcb'], 'אורן', '0512345678')
+    ''')
     return conn
 
 @app.route("/")
 def index():
     conn = db_connection()
     cur = conn.cursor()
-    cur.execute('''SELECT * from apartments''')
+    cur.execute('''SELECT * from post_apart''')
     data = cur.fetchall()
     cur.close()
     conn.close()
@@ -38,10 +61,12 @@ def create():
     ap_data = request.json 
     
     whichAct = ap_data.get('whichAction')
+    userOwnerId = ap_data.get('userOwnerId')
     apType = ap_data.get('apType')
     apCity = ap_data.get('apCity')
     apStreet = ap_data.get('apStreet')
     numOfRooms = ap_data.get('numOfRooms')
+    parkingNum = ap_data.get('parkingNum')
     moreDesc = ap_data.get('moreDesc')
     builtInMeter = ap_data.get('builtInMeter')
     price = ap_data.get('price')
@@ -52,11 +77,11 @@ def create():
 
     # SQL query with parameter placeholders
     query = '''
-    INSERT INTO apartments (whichAction, apType, apCity, apStreet, numOfRooms, moreDesc, builtInMeter, price, availDate, apImages, ownerName, ownerPhone)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    INSERT INTO post_apart (whichAction, userOwnerId ,apType, apCity, apStreet, numOfRooms, parkingNum, moreDesc, builtInMeter, price, availDate, apImages, ownerName, ownerPhone)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
     '''
     try:
-        cur.execute(query, (whichAct, apType, apCity, apStreet, numOfRooms, moreDesc, builtInMeter, price, availDate, apImages, ownerName, ownerPhone))
+        cur.execute(query, (whichAct, userOwnerId, apType, apCity, apStreet, numOfRooms, parkingNum, moreDesc, builtInMeter, price, availDate, apImages, ownerName, ownerPhone))
         conn.commit()
         cur.close()
         conn.close()
@@ -79,10 +104,12 @@ def update():
     
     # Get the values
     whichAct = ap_data.get('whichAction')
+    userOwnerId = ap_data.get('userOwnerId')
     apType = ap_data.get('apType')
     apCity = ap_data.get('apCity')
     apStreet = ap_data.get('apStreet')
     numOfRooms = ap_data.get('numOfRooms')
+    parkingNum = ap_data.get('parkingNum')
     moreDesc = ap_data.get('moreDesc')
     builtInMeter = ap_data.get('builtInMeter')
     price = ap_data.get('price')
@@ -94,10 +121,10 @@ def update():
 
     try:
         cur.execute('''
-            UPDATE apartments 
-            SET whichAction=%s, apType=%s, apCity=%s, apStreet=%s, numOfRooms=%s, moreDesc=%s, builtInMeter=%s, price=%s, availDate=%s, apImages=%s, ownerName=%s, ownerPhone=%s 
+            UPDATE post_apart 
+            SET whichAction=%s, userOwnerId=%s ,apType=%s, apCity=%s, apStreet=%s, numOfRooms=%s, parkingNum=%s, moreDesc=%s, builtInMeter=%s, price=%s, availDate=%s, apImages=%s, ownerName=%s, ownerPhone=%s 
             WHERE id=%s
-        ''', (whichAct, apType, apCity, apStreet, numOfRooms, moreDesc, builtInMeter, price, availDate, apImages, ownerName, ownerPhone, id))
+        ''', (whichAct, userOwnerId, apType, apCity, apStreet, numOfRooms, parkingNum, moreDesc, builtInMeter, price, availDate, apImages, ownerName, ownerPhone, id))
         
         conn.commit()
         cur.close()
@@ -124,7 +151,7 @@ def delete():
         ap_data = request.json  
         id = ap_data.get("id")
         
-        cur.execute('''DELETE FROM apartments WHERE id=%s''', (id,))  
+        cur.execute('''DELETE FROM post_apart WHERE id=%s''', (id,))  
         conn.commit()
         cur.close()
         conn.close()
@@ -138,6 +165,13 @@ def delete():
 
     response = make_response(jsonify(response_data), status_code)
     return response
+
+@app.route("/newPost", methods=['GET'])
+def testPost():
+    tempPost = {
+        "message":"Hello Test Func Auth"
+    }
+    return jsonify(tempPost)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5002)
--- apartment_service/db.py
+++ apartment_service/db.py
@@ -3,9 +3,28 @@ import psycopg2
 conn = psycopg2.connect(database="flask_db",host="postgres",user="postgres",password="yosefha4",port="5432")
 cursor = conn.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS apartments(id serial PRIMARY KEY,whichAction varchar(50), apType varchar(50),apCity varchar(50), apStreet varchar(50), numOfRooms integer, moreDesc varchar(100), builtInMeter integer, price varchar(50), availDate varchar(50), apImages varchar[], ownerName varchar(50), ownerPhone varchar(50))''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS apartments (
+  id serial PRIMARY KEY,
+  whichAction varchar(50),
+  userOwnerId varchar(400),
+  apType varchar(50),
+  apCity varchar(50),
+  apStreet varchar(50),
+  numOfRooms integer,
+  parkingNum integer,
+  moreDesc varchar(100),
+  builtInMeter integer,
+  price varchar(50),
+  availDate varchar(50),
+  apImages varchar[],
+  ownerName varchar(50),
+  ownerPhone varchar(50)
+);
+''')
 
-cursor.execute('''INSERT INTO apartments (whichAction,apType,apCity,apStreet,numOfRooms,moreDesc,builtInMeter,price,availDate,apImages,ownerName,ownerPhone) VALUES ('Rent','House','Netivot','Smilo St',4,'Excellent house, well equipped, quiet neighbors',185,'3,500₪','10/10/2023',ARRAY ['firstHosuseImgURL','secondHouseImgURL'],'Yosef Tash','0512345678') ''')
+cursor.execute('''INSERT INTO apartments (whichAction, userOwnerId ,apType, apCity, apStreet, numOfRooms, parkingNum, moreDesc, builtInMeter, price, availDate, apImages, ownerName, ownerPhone)
+VALUES ('מכירה', 'testu2s2erid22owner22', 'דירה', 'תל אביב', 'קינג גורג', 5, 1, 'קומה 3, נוף לים', 170, '2999999', '1/12/2023', ARRAY['abcc','abcb'], 'אורן', '0512345678')
+ ''')
 
 conn.commit()
 cursor.close()
